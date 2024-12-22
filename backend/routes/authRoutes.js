5608c1f51665ed4e5b81b354d274e70f5b26f85c
ahlilfikri <128287432+ahlilfikri@users.noreply.github.com>
2024-12-23 00:58:27 +0700

    fix conflict

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,10 +3,15 @@ const express = require('express');
 const router = express.Router();
 const { protect } = require('../middleware/auth');  // Import protect middleware
 const authController = require('../controllers/authController');
-const { validateRegistration, validateLogin } = require('../middleware/validation');
+const { validateRegistration, validateLogin, validatePasswordReset } = require('../middleware/validation');
 
 // Public routes (tidak perlu login)
 router.post('/register', validateRegistration, authController.register);
 router.post('/login', validateLogin, authController.login);
+router.post('/forgot-password', authController.forgotPassword);
+router.post('/verify-reset-token/:token', authController.verifyResetToken);
+// Protected route (perlu login)
+router.post('/reset-password', protect, validatePasswordReset, authController.resetPassword);
+router.get('/detail/:email', protect, authController.getUserByEmail);
 
 module.exports = router;
